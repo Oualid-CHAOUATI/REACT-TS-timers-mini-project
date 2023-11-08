a60cbe9dbb2c79d9f7353298d1c012d514d6cc80
waleed <oualidchaouati3@gmail.com>
2023-11-08 18:55:12 +0100

    handle  with a ref

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { TTimer, useTimersContext } from "../store/Timers-ctx.tsx";
 import Container from "./UI/Container.tsx";
 
-const TIMER_INTEVAL = 50;
+const TIMER_INTEVAL = 1000;
 export default function TimerList({ name, duration }: TTimer) {
   //duration en s ==> convertir en ms
   const [remainingTime, setRemainingTimer] = useState(duration * 1000);
@@ -14,12 +14,11 @@ export default function TimerList({ name, duration }: TTimer) {
 
   if (isRunning && start == null) setStart(Date.now());
 
-  // const interval = useRef<number>(null);
+  const interval = useRef<number | null>(null);
 
-  let timer: number;
   useEffect(() => {
     function clear() {
-      clearInterval(timer);
+      if (interval.current) clearInterval(interval.current);
     }
 
     if (!isRunning) return clear();
@@ -29,7 +28,7 @@ export default function TimerList({ name, duration }: TTimer) {
       return clear();
     }
 
-    timer = setInterval(() => {
+    interval.current = setInterval(() => {
       setRemainingTimer((t) => {
         const newRemaining = t - TIMER_INTEVAL;
         if (newRemaining <= 0 && end === null) {
@@ -38,6 +37,8 @@ export default function TimerList({ name, duration }: TTimer) {
         return newRemaining > 0 ? newRemaining : 0;
       });
     }, TIMER_INTEVAL);
+
+    console.log(interval.current);
 
     return clear;
   }, [isRunning, end]);
